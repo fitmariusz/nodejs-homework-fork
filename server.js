1665dc643a352e2bba41d2678f1c2b7587c75a33
fitmariusz <fitmariusz@gmail.com>
2024-07-07 21:08:03 +0200

    Start_new_version

--- server.js
+++ server.js
@@ -1,5 +1,56 @@
-const app = require('./app')
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose');
+const apiRouter = require('./api');
+const dotenv = require('dotenv')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+// const contactsRouter = require('./routes/api/contacts')
+
+
+dotenv.config()
+
+const { DB_HOST: urlDb } = process.env;
+console.log(urlDb);
+const connection = mongoose.connect(urlDb);
+
+const app = express()
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api', apiRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: `Not found - ${req.path}` });
 })
+
+app.use((err, req, res, next) => {
+    if(err.name === 'ValidationError'){
+        return res.status(400).json({ message: err.message });
+    } else {
+        res.status(500).json({ message: err.message || 'Something went wrong' });
+    }
+})
+
+const startServer = async () => {
+    try{
+        await connection;
+        console.log('Database connected');
+        app.listen(8000, () => {
+            console.log('Server started on http://localhost:8000');
+        });
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+startServer();
+
+
+
